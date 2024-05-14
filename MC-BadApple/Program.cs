6ce89743facfc50c81c9a6b6883ca4d94a775f9d
potesala <110117095+tamago572@users.noreply.github.com>
2024-05-15 02:54:00 +0900

    Minecraftへの接続処理を実装

--- MC-BadApple/Program.cs
+++ MC-BadApple/Program.cs
@@ -1,11 +1,37 @@
 ﻿using MinecraftConnection;
+using OpenCvSharp;
+using System;
 
-string address = "localhost";
-ushort port = 25575;
-string pswd = "minecraft";
+
+string address = "localhost"; // サーバーのアドレス
+ushort port = 25575; // RCONのポート番号
+string pswd = "minecraft"; // RCONのパスワード
 MinecraftCommands command = new MinecraftCommands(address, port, pswd);
 
-command.DisplayTitle("Hello, World!");
+string videoPath = "/Users/potesala/badapple-48x.mp4"; // 再生する動画のパス
+
+command.SetSubTitle("feat. nomico");
+command.DisplayTitle("Bad Apple!!");
+
+using(var capture = new VideoCapture(videoPath))
+{
+    if(!capture.IsOpened())
+    {
+        Console.WriteLine("動画ファイルが開けませんでした");
+        return;
+    }
+    var img = new Mat();
+    while(true)
+    {
+        if (capture.Read(img))
+        {
+            Cv2.ImShow("Bad Apple!!", img);
+            Cv2.WaitKey(1000 / 30); // 1フレームあたりの時間 (30fps)
+        } else {
+            break; // 動画の最後まで再生したら終了 (フレームが空のため終了する)
+        }
+    }
+}
 
 // See https://aka.ms/new-console-template for more information
 Console.WriteLine("Hello, World!");
---
+++ MC-BadApple/README.md
@@ -0,0 +1,9 @@
+# MC-BadApple
+
+MinecraftでBad Apple!!のPVをブロックで再現するスクリプト
+
+サーバーのRCON機能を活用する
+
+OpenCVで画素の色情報を取得し、色によってMinecraftにSetBlockコマンドを送信する
+
+画像処理コードは基本0or1-movie-cppリポジトリをC#で書き直すだけ
