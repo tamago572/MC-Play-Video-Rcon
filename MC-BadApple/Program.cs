e7aa4ba2b24dffc4c4a844aeb5a7243b9bd3ba58
potesala <110117095+tamago572@users.noreply.github.com>
2024-05-15 02:09:05 +0900

    first commit

---
+++ MC-BadApple/Program.cs
@@ -0,0 +1,12 @@
+﻿using MinecraftConnection;
+
+string address = "localhost";
+ushort port = 25575;
+string pswd = "minecraft";
+MinecraftCommands command = new MinecraftCommands(address, port, pswd);
+
+command.DisplayTitle("Hello, World!");
+
+// See https://aka.ms/new-console-template for more information
+Console.WriteLine("Hello, World!");
+
